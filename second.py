2533e6d2cb44c8a02a5091e09addaf1b2c9c5938
Matilda Sjöblom <matilda.sjo96@gmail.com>
2018-12-04 19:38:17 +0100

    second day done

--- second.py
+++ second.py
@@ -8,6 +8,7 @@ myfile.close()
 c = [0, 0]
 for i in contents:
     a = [j for i, j in Counter(i).most_common()]
+
     if 3 in a:
         c[0] += 1
     if 2 in a:
@@ -15,3 +16,21 @@ for i in contents:
 
 
 print(c[0] * c[1])
+
+count = 0
+result = ''
+
+for x in range(len(contents) - 1):
+    tmp = contents[x]
+    for y in range(x+1, len(contents), 1):
+        tmp2 = contents[y]
+        for z in range(len(tmp)):
+            if tmp[z] == tmp2[z]:
+                count += 1
+                result = result + tmp[z]
+
+                if count == (len(tmp) - 1):
+                    print(result)
+        result = ''
+        count = 0
+
