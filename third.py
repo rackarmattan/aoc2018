cd6ba74e34fa0af1930926829ecfdca0e4d856e7
Matilda Sjöblom <matilda.sjo96@gmail.com>
2018-12-05 21:37:05 +0100

    third day done

--- third.py
+++ third.py
@@ -4,98 +4,46 @@ myfile = open('3.txt', 'r')
 contents = myfile.read().strip().splitlines()
 myfile.close()
 
+def marginLeft(x):
+    return int(x[x.find(' ')+2:x.find(',')])
+
+def marginTop(x):
+    return int(x[x.find(',')+1:x.find(':')])
+
+def get_width(x):
+    return int(x[x.find(':') + 2:x.find('x')])
+
+def get_height(x):
+    return int(x[x.find('x')+1:len(x)])
+
+def get_id(x):
+    return int(x[1:x.find(' ')])
+
 def matrixSize():
     matrixHeight = 0
     matrixWidth = 0
     for x in contents:
-        leftPointer = 0
-        rightPointer = 0
-        while x[leftPointer] != ' ':
-            leftPointer += 1
 
-        leftPointer = leftPointer + 3
-        rightPointer = leftPointer
-
-        while x[rightPointer] != ',':
-            rightPointer += 1
-
-        # hur långt från vänster den ska börrja
-        leftMargin = int(x[leftPointer:rightPointer])
-
-        # print('left margin: ', leftMargin)
-
-        rightPointer += 1
-        leftPointer = rightPointer
-
-        while x[rightPointer] != ':':
-            rightPointer += 1
-
-        # hur långt från toppen den ska börja
-        topMargin = int(x[leftPointer:rightPointer])
-        # print('top margin ', topMargin)
-
-        rightPointer += 2
-        leftPointer = rightPointer
-
-        while x[rightPointer] != 'x':
-            rightPointer += 1
-
-        width = int(x[leftPointer:rightPointer])
-
-        # print('width ', width)
-
-        rightPointer += 1
-        leftPointer = rightPointer
-
-        height = int(x[leftPointer:len(x)])
-
-        if leftMargin + width > matrixWidth:
-            matrixWidth = leftMargin + width
-        if topMargin + height > matrixHeight:
-            matrixHeight = topMargin + height
+        if marginLeft(x) + get_width(x) > matrixWidth:
+            matrixWidth = marginLeft(x) + get_width(x)
+        if marginTop(x) + get_height(x) > matrixHeight:
+            matrixHeight = marginTop(x) + get_height(x)
 
     return(matrixWidth, matrixHeight)
-
 
 matrix = numpy.zeros(matrixSize())
 
 for x in contents:
-    leftPointer = 0
-    rightPointer = 0
-    while x[leftPointer] != ' ':
-        leftPointer += 1
 
-    leftPointer = leftPointer + 3
-    rightPointer = leftPointer
+    id = get_id(x)
 
-    while x[rightPointer] != ',':
-        rightPointer += 1
+    leftMargin = marginLeft(x)
 
-    # hur långt från vänster den ska börrja
-    leftMargin = int(x[leftPointer:rightPointer])
+    topMargin = marginTop(x)
 
+    width = get_width(x)
 
-    rightPointer += 1
-    leftPointer = rightPointer
-
-    while x[rightPointer] != ':':
-        rightPointer += 1
-
-    # hur långt från toppen den ska börja
-    topMargin = int(x[leftPointer:rightPointer])
-
-    rightPointer += 2
-    leftPointer = rightPointer
-
-    while x[rightPointer] != 'x':
-        rightPointer += 1
-
-    width = int(x[leftPointer:rightPointer])
-
-    rightPointer += 1
-    leftPointer = rightPointer
-
-    height = int(x[leftPointer:len(x)])
+    height = get_height(x)
 
     for row in range(topMargin, topMargin + height, 1):
         for col in range(leftMargin, leftMargin + width, 1):
@@ -107,6 +55,29 @@ for i in range(len(matrix)):
         if matrix[i][j] >= 2:
             count += 1
 print(count)
+
+
+for x in contents:
+    id = get_id(x)
+
+    leftMargin = marginLeft(x)
+
+    topMargin = marginTop(x)
+
+    width = get_width(x)
+
+    height = get_height(x)
+
+    area = width*height
+    checkArea = 0
+
+    for row in range(topMargin, topMargin + height, 1):
+        for col in range(leftMargin, leftMargin + width, 1):
+            checkArea += matrix[row][col]
+
+    if checkArea == area:
+        print(id)
+        break
 
 
 
